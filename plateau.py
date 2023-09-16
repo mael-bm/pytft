0b9e2bbb90e4bc61753db1651f9e9b50692ee99c
ynot <maelbarreremusset@fastmail.fm>
2023-09-16 18:12:36 +0200

    Update plateau.py

--- plateau.py
+++ plateau.py
@@ -1,11 +1,16 @@
 from typing import Tuple
+
 # milieu case de depart x:560 y:450
 INDEX_X = 560
 INDEX_Y = 450
+
+
 def get_coords(x: int, y: int) -> Tuple[int, int]:
-    x, y = x-1, y-1
-    larg_x = 115
-    new_x = INDEX_X + x*larg_x + int(larg_x/2)*(y%2)
-    new_y = INDEX_Y + y*75
+    x, y = x - 1, y - 1
+    size_x = 117
+    new_x = INDEX_X + x * size_x + int(size_x / 2) * (y % 2)
+    new_y = INDEX_Y + y * 75
     return new_x, new_y
-print(get_coords(3, 4))
+
+
+print(get_coords(7, 4))
